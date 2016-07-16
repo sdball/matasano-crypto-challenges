1283b4f06c62524aa5228ef37d003a2b2fbf95a8
Stephen Ball <sdball@gmail.com>
2016-07-15 22:42:55 -0400

    Simplify Elixir Set1/Challenge1

--- elixir/set1/challenge1/lib/challenge1.ex
+++ elixir/set1/challenge1/lib/challenge1.ex
@@ -1,15 +1,7 @@
 defmodule Challenge1 do
   def convert_hex_to_base64(string) do
     string
-    |> decode_hex
-    |> encode_base64
-  end
-
-  defp decode_hex(string) do
-    Base.decode16!(string, case: :mixed)
-  end
-
-  defp encode_base64(string) do
-    Base.encode64(string)
+    |> Base.decode16!(case: :mixed)
+    |> Base.encode64
   end
 end
