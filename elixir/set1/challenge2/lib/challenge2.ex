a7131d1f138bfa2f53a9004ef95a9c2f8cfbaf34
Stephen Ball <sdball@gmail.com>
2016-07-16 21:14:31 -0400

    Simplify Elixir Set1/Challenge2

--- elixir/set1/challenge2/lib/challenge2.ex
+++ elixir/set1/challenge2/lib/challenge2.ex
@@ -1,34 +1,21 @@
 defmodule Challenge2 do
   use Bitwise
 
-  @doc """
-  Performs bit-wise XOR (exclusive or) on the given binaries `left` and `right`.
-
-  The two arguments must be the same byte size.
-  """
   def fixed_xor(left, right) do
-    left = decode_hex(left)
-    right = decode_hex(right)
+    left = Base.decode16!(left, case: :mixed)
+    right = Base.decode16!(right, case: :mixed)
 
     # :crypto.exor(left, right)
     do_fixed_xor(left, right, "")
-    |> encode_hex
+    |> Base.encode16(case: :lower)
   end
 
-  defp do_fixed_xor("", "", output) do
-    output
+  defp do_fixed_xor("", "", result) do
+    result
   end
 
-  defp do_fixed_xor(<<l_head, l_tail::binary>>, <<r_head, r_tail::binary>>, output) do
-    next_byte = <<bxor(l_head, r_head)>>
-    do_fixed_xor(l_tail, r_tail, output <> next_byte)
-  end
-
-  defp decode_hex(string) do
-    Base.decode16!(string, case: :mixed)
-  end
-
-  defp encode_hex(string) do
-    Base.encode16(string, case: :lower)
+  defp do_fixed_xor(<<left_byte, left::binary>>, <<right_byte, right::binary>>, result) do
+    transformed_byte = <<bxor(left_byte, right_byte)>>
+    do_fixed_xor(left, right, result <> transformed_byte)
   end
 end
